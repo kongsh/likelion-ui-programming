c74e1f5b82af848bf11ee5561b22a0c5eaea4921
kongsh <ksh080838@gmail.com>
2025-01-09 15:20:03 +0900

    [실습] React.Fragment (조각, 파편) 빌트인 컴포넌트
    - 실제 DOM 렌더링 안함
    - <template> 요소와 유사

--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -2,9 +2,6 @@
 import React from "../lib/react.js";
 import ReactDOM from "../lib/react-dom/client.js";
 
-// console.log("React version = ", React.version);
-// console.log("ReactDOM version = ", ReactDOM.version);
-
 // Creating React Element Node
 
 // React API
@@ -20,7 +17,23 @@ const changeBtn = React.createElement(
   "인사말"
 );
 
-console.group("React 요소 노드");
-console.dir(heading);
-console.dir(changeBtn);
-console.groupEnd();
+// 의미 없는 <div> 대신 <React.Fragment>를 사용
+// 실제 DOM에 렌더링 되지 않는 Fragment
+
+const wrapper = React.createElement(
+  React.Fragment,
+  null,
+  // ...children === React.ReactNode[]
+  heading, // child 1
+  changeBtn // child 2
+);
+
+// ReactDOM API (for Web)
+// ReactDOM.createRoot(domElement) -> ReactDOMRoot { render, unmount }
+const rootElement = document.getElementById("react"); // HTMLDivElement
+const reactDomRoot = ReactDOM.createRoot(rootElement); // ReactDOMRoot
+
+// ReactDOMRoot.render(React.ReactElement)
+reactDomRoot.render(wrapper);
+
+// React Navite API (for Mobile Native)
