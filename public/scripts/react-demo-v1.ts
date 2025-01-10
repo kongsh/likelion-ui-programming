c61c7ae575528f012029ad37024d309ec53fde6a
kongsh <ksh080838@gmail.com>
2025-01-10 09:23:35 +0900

    [정리] 리액트 엘리먼트와 컴포넌트

---
+++ public/scripts/react-demo-v1.ts
@@ -0,0 +1,19 @@
+import React from "../lib/react.js";
+import ReactDOM from "../lib/react-dom/client.js";
+
+const heading = React.createElement("h1", {}, "안녕!");
+
+const changeBtn = React.createElement(
+  "button",
+  {
+    type: "button",
+  },
+  "인사말"
+);
+
+const wrapper = React.createElement(React.Fragment, null, heading, changeBtn);
+
+const rootElement = document.getElementById("react");
+const reactDomRoot = ReactDOM.createRoot(rootElement);
+
+reactDomRoot.render(wrapper);
